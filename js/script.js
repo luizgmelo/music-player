c133a5fc15a7b5579ab433143ffe9f0e160592db
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-01-29 12:33:49 -0300

    fix: images github pages

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 const musics = [
   {
     id: "1",
-    logoSrc: "../images/logo.jpeg",
+    logoSrc: "./images/logo.jpeg",
     name: "505",
-    musicSrc: "../musics/505.mp3"
+    musicSrc: "./musics/505.mp3"
   },
   {
     id: "2",
-    logoSrc: "../images/logo.jpeg",
+    logoSrc: "./images/logo.jpeg",
     name: "I Wanna Be Yours",
     musicSrc: "../musics/I Wanna Be Yours.mp3"
   },
   {
     id: "3",
-    logoSrc: "../images/logo.jpeg",
+    logoSrc: "./images/logo.jpeg",
     name: "Do I Wanna Know",
     musicSrc: "../musics/Do I Wanna Know.mp3"
   }
